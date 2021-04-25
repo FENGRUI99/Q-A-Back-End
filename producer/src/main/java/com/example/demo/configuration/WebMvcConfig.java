8e5c5a944798a9d37c36692d0c0fe472f3593a42
l1 <puxuan.huang18@student.xjtlu.edu.cn>
2021-04-25 18:37:45 +0800

    1

--- producer/src/main/java/com/example/demo/configuration/WebMvcConfig.java
+++ producer/src/main/java/com/example/demo/configuration/WebMvcConfig.java
@@ -19,6 +19,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/**").addResourceLocations("/temp-rainy/");
+        registry.addResourceHandler("/static/image/**").addResourceLocations("/temp-rainy/");
     }
 }
