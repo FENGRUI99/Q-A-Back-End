a1d37a1ca79384b87fbc928fcde9493303401566
l1 <puxuan.huang18@student.xjtlu.edu.cn>
2021-04-28 15:12:31 +0800

    1

--- producer/src/main/java/com/example/demo/service/service/EmailVerifyService.java
+++ producer/src/main/java/com/example/demo/service/service/EmailVerifyService.java
@@ -3,6 +3,5 @@ package com.example.demo.service.service;
 import com.example.demo.configuration.ResponseMessage;
 
 public interface EmailVerifyService {
-
     ResponseMessage sendEmail(String user_mail);
 }
