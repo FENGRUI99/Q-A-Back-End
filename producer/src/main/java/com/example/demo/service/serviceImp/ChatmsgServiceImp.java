8517502481e89320abea4d4bb1c61d68acb2c562
l1 <puxuan.huang18@student.xjtlu.edu.cn>
2021-05-12 15:47:16 +0800

    ShiroConfig.java

---
+++ producer/src/main/java/com/example/demo/service/serviceImp/ChatmsgServiceImp.java
@@ -0,0 +1,14 @@
+package com.example.demo.service.serviceImp;
+
+import com.example.demo.pojo.ChatMsg;
+import com.example.demo.service.service.ChatmsgService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChatmsgServiceImp {
+
+}
